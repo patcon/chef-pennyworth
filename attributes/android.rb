7183197f571d7c8ea0d4096a04270494c2278217
Patrick Connolly <patrick@myplanetdigital.com>
2012-01-02 11:43:27 -0400

    Bumped to 0.2.2

--- attributes/android.rb
+++ attributes/android.rb
@@ -1,2 +1,5 @@
 default[:pennyworth][:android_sdk_url] = "http://dl.google.com/android/android-sdk_r15-linux.tgz"
 default[:pennyworth][:android_path] = "/opt/android-sdk-linux"
+
+default[:pennyworth][:android][:avd] = "BBDdevice"
+default[:pennyworth][:android][:target] = 3
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Configures the pennyworth continuous deployment pipeline system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.1"
+version          "0.2.2"
 
 depends "java"
 depends "jenkins"
@@ -11,3 +11,4 @@ depends "postfix"
 depends "build-essential"
 depends "reprepro"
 depends "leiningen"
+depends "ant"
